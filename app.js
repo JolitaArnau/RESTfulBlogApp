1d6e30ce5ec071abffb6006c82796c63f756a18b
Jolita Arnaudova <jolita.arnaudova@gmail.com>
2017-10-29 17:52:14 +0100

    added express-sanitizer, in order to validate user input when creating and updating posts, so that no harmful script tags can be included

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var express         = require("express"),
-    app             = express(),
-    bodyParser      = require("body-parser"),
-    mehtodOverride  = require("method-override"),
-    mongoose        = require("mongoose");
+var express             = require("express"),
+    app                 = express(),
+    bodyParser          = require("body-parser"),
+    methodOverride      = require("method-override"),
+    expressSanitizer    = require("express-sanitizer"),
+    mongoose            = require("mongoose");
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(mehtodOverride("_method"));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 
 // MONGOOSE/MODEL CONFIG
@@ -46,6 +48,7 @@ app.get("/blogs/new", function (req, res) {
 
 // CREATE ROUTE
 app.post("/blogs", function (req, res) {
+    req.body.blog.body = expressSanitizer(req.body.blog.body);
     Blog.create(req.body.blog, function (err, newBlog) {
         if (err) {
             res.render("new");
@@ -83,6 +86,7 @@ app.get("/blogs/:id/edit", function (req,res) {
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function (req, res) {
+    req.body.blog.body = expressSanitizer(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlogPost) {
         if (err) {
             res.redirect("/blogs");
