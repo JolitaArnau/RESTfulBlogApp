81e4c3a450cd80a6f7e0ea44716b19a3e4f6c7f2
Jolita Arnaudova <jolita.arnaudova@gmail.com>
2017-10-29 16:06:08 +0100

    installed method-override package, so i can use put requests when submiting the update form, which has the purpose to let the user edit posts and save the updated data back to db

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mehtodOverride  = require("method-override"),
+    mongoose        = require("mongoose");
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(mehtodOverride("_method"));
 
 
 // MONGOOSE/MODEL CONFIG
@@ -65,6 +67,30 @@ app.get("/blogs/:id", function (req, res) {
        }
 
    });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function (req,res) {
+    Blog.findById(req.params.id, function (err, foundBlogPost) {
+        if (err) {
+            res.redirect("/blogs");
+        }
+        else {
+            res.render("edit", {blog: foundBlogPost});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function (req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlogPost) {
+        if (err) {
+            res.redirect("/blogs");
+        }
+        else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
 });
 
 app.listen(3000, function () {
